cf075766b9d94ceaaab6e5e83d959adb271c12a5
Hector <hamgalex@hotmail.com>
2021-06-17 10:45:06 -0500

    Se añadió el fetch de la palabra con api de diccionario

--- test.js
+++ test.js
@@ -1,35 +1,38 @@
 
 
 function buscar() {
+
     let palabra = document.getElementById("search").value.toLowerCase();
     apiURL =  "https://api.dictionaryapi.dev/api/v2/entries/es/"+ palabra;
-    
-    document.getElementById("resultado").innerHTML = apiURL;
-    encontrarPalabra();
 
+    encontrarPalabra();
 }
 
-function encontrarPalabra()
+function encontrarPalabra(){
+            
+    var data = undefined;
+    var request = new XMLHttpRequest();
+
+    request.open('GET', apiURL, true);
+    request.send();
+
+    request.onreadystatechange = function ()
+    {      
+        if (this.readyState == 4 && this.status == 200)
         {
-            
-            var data = undefined;
-            var request = new XMLHttpRequest();
-            request.open('GET', currentApiURL, true);
-            request.send();
-
-            request.onreadystatechange = function ()
-            {
-               
-                if (this.readyState == 4 && this.status == 200)
-                {
-                    debugger;
-            
-                    var resultRawData =  this.response;
-                    data =  JSON.parse(resultRawData);
-                    showApiData(data);
-
-                }
-
-            }
-
+            var resultRawData =  this.response;
+            data =  JSON.parse(resultRawData);
+            console.log(data);
+            mostrarDatos(data);
         }
+        else{
+            document.getElementById("resultado").innerHTML ="No se encontró la palabra";
+        }
+    }
+}
+
+function mostrarDatos(data)
+{
+    
+    document.getElementById("resultado").innerHTML = "<strong>Definición</strong>: "+data[0].meanings[0].definitions[0].definition;
+}
